bd2bcb48fbef5a798e873139c9bf79965d76562e
innocent <okamainnocent2020@gmail.com>
2022-07-26 23:17:40 +0100

    added provider folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/product_provider.dart';
+import 'package:shop_app/screens/product_detail_screen.dart';
+import 'package:shop_app/screens/product_overview_screen.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (ctx) => ProductProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'MyShop',
+        theme: ThemeData(
+            primarySwatch: Colors.pink,
+            accentColor: Colors.deepOrange,
+            fontFamily: 'Lato'),
+        initialRoute: '/',
+        routes: {
+          '/': (ctx) => ProductsOverviewScreen(),
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('MyShop'),
-      ),
-      body: Center(
-        child: Text('Let\'s build a shop!'),
-      ),
-    );
-  }
-}
+// class MyHomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('MyShop'),
+//       ),
+//       body: Center(
+//         child: Text('Let\'s build a shop!'),
+//       ),
+//     );
+//   }
+// }
---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/product_provider.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  // final String title;
+  // final double price;
+
+  // ProductDetailScreen(this.title, this.price);
+  static const routeName = '/product-detail';
+
+  @override
+  Widget build(BuildContext context) {
+    final productId =
+        ModalRoute.of(context)!.settings.arguments as String; // is the id!
+    final loadedProduct = Provider.of<ProductProvider>(
+      context,
+      listen: false,
+    ).findById(productId);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(loadedProduct.title),
+      ),
+    );
+  }
+}
